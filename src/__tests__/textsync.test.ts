b73e611a519111e4f78fe6f5a491e2641d9fbb99
James Lees <james.lees@pusher.com>
2018-01-24 12:04:24 +0000

    Include some unit testing

---
+++ src/__tests__/textsync.test.ts
@@ -0,0 +1,30 @@
+import {} from 'jest';
+
+import TextSync from '../lib/textsync';
+import { Permissions } from '../lib/permissions';
+
+import { Instance } from 'pusher-platform-node';
+
+const locator = 'v1:cluster:some-long-id';
+const key = 'id:secret';
+
+describe('textsync', () => {
+  let textsync;
+  beforeEach(() => {
+    textsync = new TextSync({
+      locator,
+      key
+    });
+  });
+  it('should provide a token object', () => {
+    const request = { documentId: 'my excellent document' };
+    const permissionsFn = documentId => Promise.resolve([Permissions.READ]);
+
+    return textsync.authorizeDocument(request, permissionsFn).then(res => {
+      expect(res).toHaveProperty('access_token', expect.any(String));
+      expect(res).toHaveProperty('refresh_token', expect.any(String));
+      expect(res).toHaveProperty('expires_in'), expect.any(Number);
+      expect(res).toHaveProperty('token_type', expect.any(String));
+    });
+  });
+});
